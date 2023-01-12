e98c04b71bd6be1a88f4aad19c431eec1d5e7a2b
okumbhar <okumbhar@IND-OKumbhar-MAC.local>
2023-01-12 16:14:56 +0530

    commvault connector initial commit

---
+++ commvault/constant.py
@@ -0,0 +1,18 @@
+""" Copyright start
+  Copyright (C) 2008 - 2022 Fortinet Inc.
+  All rights reserved.
+  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+  Copyright end """
+
+EXPIRY_TIME = 29  # token expiry time in minutes
+
+
+class Method:
+    GET = "GET"
+    POST = "POST"
+
+
+class Endpoint:
+    LOGIN = "/Login"
+    ALERT_RULE = "/AlertRule"
+    USER = "/User"
